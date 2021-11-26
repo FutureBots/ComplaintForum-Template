84439a48aa82c4365d71372f4716e45012d538d3
FutureBots <90165069+FutureBots@users.noreply.github.com>
2021-11-26 15:05:45 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,19 @@ require("@firebase/firestore")
 
 const firebaseConfig = {
  //Add configuration here
- 
+ const firebaseConfig = {
+  apiKey: "AIzaSyCO-LmpeVjJtyzoRIDgQHXx0gHaXpSFgB4",
+  authDomain: "complaint-forum-572e2.firebaseapp.com",
+  projectId: "complaint-forum-572e2",
+  storageBucket: "complaint-forum-572e2.appspot.com",
+  messagingSenderId: "316165041220",
+  appId: "1:316165041220:web:da1eb1f8793d0cac62e011",
+  measurementId: "G-V50XX77P5D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 };
   firebase.initializeApp(firebaseConfig);
 
